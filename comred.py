9e5499d35bd94764f3247f7b687f88d3f74c311d
Anuraj Upadhyay <anurajupadhyay6@gmail.com>
2024-10-30 20:10:32 +0530

    minor additions

--- comred.py
+++ comred.py
@@ -2,20 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-html_text = requests.get('https://www.reddit.com/r/wallpaper/hot/?feedViewType=compactView').text
-
-
 f=open("comred.txt","a+")
-soup= BeautifulSoup(html_text, 'lxml')
-st= soup.find_all('div', class_="media-lightbox-img max-h-[100vw] h-full w-full object-contain overflow-hidden relative bg-black")
-for i in st:
-    stn=i.find('img')
-    print ('---------')
-    try:
-        print (stn['src'])
-        f.write(stn['src']+'\n')
-    except:
-        print("err")
+
+L=["wallpaper","iphonexwallpapers","phonewallpapers","iWallpaper","Wallpaperdump","Wallpaperengine","iphonewallpaper","wallpapers"]
+for i in L:
+    html_text = requests.get('https://www.reddit.com/r/'+i+'/hot/?feedViewType=compactView').text
+    soup= BeautifulSoup(html_text, 'lxml')
+    st= soup.find_all('div', class_="media-lightbox-img max-h-[100vw] h-full w-full object-contain overflow-hidden relative bg-black")
+    for i in st:
+        stn=i.find('img')
+        print ('---------')
+        try:
+            print (stn['src'])
+            f.write(stn['src']+'\n')
+        except:
+            print("err")
 
 f.close()
 
