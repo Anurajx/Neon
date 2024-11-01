97e6bf4dc5693ca6bea1713b6a5df108833aec78
Anuraj Upadhyay <anurajupadhyay6@gmail.com>
2024-11-01 23:01:15 +0530

    Update fileread.js

--- fileread.js
+++ fileread.js
@@ -1,7 +1,7 @@
 async function loadImages(imagesrc) {
   try {
     const response = await fetch(
-      `https://neon-mauve.vercel.app/api/?url=${imagesrc}`
+      `https://neon-mauve.vercel.app/api/load-images?url=${imagesrc}`
     );
     const imageData = await response.blob();
     const imageURL = URL.createObjectURL(imageData);
