013e155d9d9a31a64065abadc62ca8ea3877450a
Anuraj Upadhyay <anurajupadhyay6@gmail.com>
2024-11-12 12:45:50 +0530

    json issue solved

--- excuseapinew.py
+++ excuseapinew.py
@@ -1,11 +1,14 @@
 import requests
+import json
 
 # Fetch data from a subreddit
 L=["Animewallpaper","WallpapersDoA","wallpaper","phonewallpapers","iWallpaper","Wallpaperdump","Wallpaperengine","wallpapers"]
 P=[]
 for i in L:
     response = requests.get('https://www.reddit.com/r/' + i + '/new.json', headers={'User-agent': 'Mozilla/5.0'})
-    data = response.json()
+    print(response.status_code)
+    #data = response.json()
+    data = json.loads(response.content.decode('utf-8'))
     f=open("excuseapi.txt","w+")
     # Extract image URLs
     for post in data['data']['children']:
