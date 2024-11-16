85280ce7bf25b94014daa351679220f3fab5d402
Anuraj Upadhyay <anurajupadhyay6@gmail.com>
2024-11-16 12:55:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,22 +5,16 @@ const http = require("http");
 const app = express();
 
 app.get("/load-images", (req, res) => {
-  console.log("Received request for image:", req.query.url);
   const imagesrc = req.query.url;
   if (imagesrc.startsWith("https")) {
     https
       .get(imagesrc, (response) => {
-        console.log("Received response from image URL:");
-        console.log("Status code:", response.statusCode);
-        console.log("Headers:", response.headers);
         res.set("Access-Control-Allow-Origin", "*");
         res.set(
           "Access-Control-Allow-Headers",
           "Origin, X-Requested-With, Content-Type, Accept"
         );
         res.set("Content-Type", response.headers["content-type"]);
-        console.log("Sending response to client:");
-        console.log("Headers:", res.getHeaders());
         response.pipe(res);
       })
       .on("error", (err) => {
@@ -30,17 +24,12 @@ app.get("/load-images", (req, res) => {
   } else {
     http
       .get(imagesrc, (response) => {
-        console.log("Received response from image URL:");
-        console.log("Status code:", response.statusCode);
-        console.log("Headers:", response.headers);
         res.set("Access-Control-Allow-Origin", "*");
         res.set(
           "Access-Control-Allow-Headers",
           "Origin, X-Requested-With, Content-Type, Accept"
         );
         res.set("Content-Type", response.headers["content-type"]);
-        console.log("Sending response to client:");
-        console.log("Headers:", res.getHeaders());
         response.pipe(res);
       })
       .on("error", (err) => {
