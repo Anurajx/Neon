771c90cc1c094a14562eab251db30835fd021391
Anuraj Upadhyay <anurajupadhyay6@gmail.com>
2024-11-21 10:29:08 +0530

    Update excuseapinew.py
    updated the order of wallpaper

--- excuseapinew.py
+++ excuseapinew.py
@@ -6,7 +6,7 @@ from requests.auth import HTTPBasicAuth
 # json is built into Python, no need to import it separately
 
 # Fetch data from a subreddit
-L = ["WallpapersDoA", "wallpaper", "phonewallpapers", "iWallpaper", "Wallpaperdump", "Wallpaperengine", "wallpapers", "Animewallpaper"]
+L = ["wallpaper", "phonewallpapers", "iWallpaper", "Wallpaperdump", "Wallpaperengine", "wallpapers","WallpapersDoA", "Animewallpaper"]
 P=[]
 # Open file before the loop to avoid NameError
 f = open("excuseapi.txt", "w+")
@@ -60,7 +60,7 @@ for i in L:
             'Authorization': access_token,
         }
         response = requests.get(
-            f'https://www.reddit.com/r/{i}/new.json',
+            f'https://www.reddit.com/r/{i}/hot.json',
             headers=headers,
             timeout=30
         )
